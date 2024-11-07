359d37ac74f9eb35075ff8de5456e16585f9d304
ayame@ <ayamessh@keemail.me>
2024-11-07 01:23:31 +0100

    Add files via upload

---
+++ crackme/2/keygen.py
@@ -0,0 +1,13 @@
+import os 
+
+os.system('cls')
+print("""KEY GEN
+
+
+""")
+
+strr = '604tz52h84'
+username = input('username : ')
+print(f'your password is {username}{username}{strr}')
+
+input('leave ?')
